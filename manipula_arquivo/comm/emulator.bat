e73da065c943b7f92218abeabe987a13ed3328ef
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-03-04 08:45:13 -0300

    Update emulator.bat

--- manipula_arquivo/comm/emulator.bat
+++ manipula_arquivo/comm/emulator.bat
@@ -4,11 +4,11 @@ if %1 == 1 goto list_emulator
 if %1 == 2 goto init_emulator
 
 :list_emulator
-C:\Users\user\AppData\Local\Android\Sdk\emulator\emulator -list-avds > "text\emulator.txt"
+%PATH%emulator -list-avds > "text\emulator.txt"
 goto close
 
 :init_emulator
-C:\Users\user\AppData\Local\Android\Sdk\emulator\emulator -avd %2
+%PATH%emulator -avd %2
 taskkill /f /im python.exe
 goto close
 
