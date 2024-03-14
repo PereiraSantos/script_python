e448a5588b960423af2893f82f80d4f697048989
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-03-14 16:55:46 -0300

    Update command.bat

--- manipula_arquivo/comm/command.bat
+++ manipula_arquivo/comm/command.bat
@@ -4,8 +4,12 @@ if %1 == 1 goto get_devices
 if %1 == 2 goto get_folder
 if %1 == 3 goto save_arquive
 if %1 == 4 goto get_database
-if %1 == 5 goto set_file
+if %1 == 5 goto set_file_download
 if %1 == 6 goto clean_log
+if %1 == 7 goto get_share
+if %1 == 8 goto save_arquive_shared
+if %1 == 9 goto set_file_database
+if %1 == 10 goto set_file_shared
 
 :get_devices
 adb devices > "text\temp.txt"
@@ -16,15 +20,31 @@ adb -s %2 shell ls /data/data/ > "text\folder.txt"
 goto close
 
 :save_arquive
-adb -s %2 pull /data/data/%3/databases/%4 %5 > "text\log.txt"
+adb -s %2 pull /data/data/%3/databases/%4 %5 >> "text\log.txt"
+goto close
+
+:save_arquive_shared
+adb -s %2 pull /data/data/%3/shared_prefs/%4 %5 >> "text\log.txt"
 goto close
 
 :get_database
 adb -s %2 shell ls /data/data/%3/databases/ > "text\database.txt"
 goto close
 
-:set_file
-adb -s %2 push %3 /sdcard/Download/ > "text\log.txt"
+:get_share
+adb -s %2 shell ls /data/data/%3/shared_prefs/ > "text\shared.txt"
+goto close
+
+:set_file_download
+adb -s %2 push %3 /sdcard/Download/ >> "text\log.txt"
+goto close
+
+:set_file_database
+adb -s %2 push %3 /data/data/%4/databases/ >> "text\log.txt"
+goto close
+
+:set_file_shared
+adb -s %2 push %3 /data/data/%4/shared_prefs/ >> "text\log.txt"
 goto close
 
 :clean_log
