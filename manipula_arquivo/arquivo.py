1addc522feb32d68a742f6e8935b4cec775050ad
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-03-14 16:55:10 -0300

    Update arquivo.py

--- manipula_arquivo/arquivo.py
+++ manipula_arquivo/arquivo.py
@@ -12,7 +12,8 @@ emuladors = []
 devices = []
 projectList = []
 databaseList = []
-typeTask = [("Enviar", 1),("Receber", 2)]
+sharedList = []
+typeTask = [("Enviar para download", 1),("Enviar para database", 2), ("Enviar para shared preference", 3), ("Puxa para pasta local", 4)]
 
 checkbuttonValue = tk.BooleanVar(value=True)
 
@@ -20,6 +21,7 @@ task = IntVar()
 
 selectProjectUser = StringVar()
 selectDatabaseUser = StringVar()
+selectSharedUser = StringVar()
 device = StringVar()
 
 folder = StringVar(value="Nenhum arquivo selecionado.")
@@ -29,7 +31,7 @@ buttonReset = StringVar(value="Resetar")
 
 
 def init_view():
-    window.geometry('550x200')
+    window.geometry('550x240')
     window.resizable(False, False)
     window.iconbitmap(r'icon\icon.ico')
     window.title('Manipular arquivo')
@@ -55,16 +57,16 @@ def init_view():
         project_radio.place(x=10, y=i)
 
     change_button = ttk.Button(textvariable=buttonSelect,command=open_folder)
-    change_button.place(x=10, y=130)
+    change_button.place(x=10, y=170)
 
     label_folder = ttk.Label(window, textvariable=folder)
-    label_folder.place(x=10, y=165)
+    label_folder.place(x=10, y=195)
 
     change_button = ttk.Button(textvariable=buttonConclud, command=load_database_device)
-    change_button.place(x=440, y=130)
+    change_button.place(x=445, y=170)
 
     change_button = ttk.Button(textvariable=buttonReset, command=reset)
-    change_button.place(x=350, y=130)
+    change_button.place(x=350, y=170)
 
     window.mainloop()
 
@@ -75,19 +77,36 @@ def reset():
 
 def build_combo_database(disabled):
     ckeck_database =  ttk.Label(window, text="Selecione o banco")
-    ckeck_database.place(x=220, y=60)
+    ckeck_database.place(x=220, y=55)
     
     combo_database = ttk.Combobox(values=databaseList, state=disabled)
     combo_database.bind("<<ComboboxSelected>>", selection_database)
-    combo_database.place(x=220, y=80, width=300)
+    combo_database.place(x=220, y=75, width=300)
+
+def build_combo_shared(disabled):
+    ckeck_database =  ttk.Label(window, text="Selecione o shared")
+    ckeck_database.place(x=220, y=100)
+    
+    combo_database = ttk.Combobox(values=sharedList, state=disabled)
+    combo_database.bind("<<ComboboxSelected>>", selection_shared)
+    combo_database.place(x=220, y=120, width=300)
 
 def select_task():
+    buttonSelect.set("Selecionar arquivo")
+    folder.set('Nenhum arquivo selecionado.')
+
     if task.get() == 1:
-        buttonSelect.set("Selecionar arquivo")
-        folder.set('Nenhum arquivo selecionado.')
         show_select_project('disabled')
         build_combo_database('disabled')
-
+        build_combo_shared('disabled')
+    elif task.get() == 2:
+        show_select_project('normal')
+        build_combo_database('disabled')
+        build_combo_shared('disabled')
+    elif task.get() == 3:
+        show_select_project('normal')
+        build_combo_database('disabled')
+        build_combo_shared('disabled')
     else:
         buttonSelect.set("Selecionar pasta")
         folder.set('Nenhum arquivo selecionado.')
@@ -103,7 +122,7 @@ def show_select_project(disabled):
 
 def open_folder():
     buttonReset.set("Resetar")
-    if task.get() == 1:
+    if task.get() != 4:
         filename = askopenfilename() 
         folder.set('Enviar '+filename)
     else:
@@ -120,27 +139,42 @@ def selection_changed(event):
 
 def selection_project(event):
     selectProjectUser.set(event.widget.get())
-    list_database_project()
+    if task.get() == 4:
+        list_database_project()
+        list_shared_project()        
 
 def selection_database(event):
     selectDatabaseUser.set(event.widget.get())
 
+def selection_shared(event):
+    selectSharedUser.set(event.widget.get())
+
 def load_database_device():
-    if checkbuttonValue.get() == True and task.get() == 2:
+    clean_log()
+
+    if checkbuttonValue.get() == True and task.get() == 4:
         cmd = 'comm\command.bat 3 '+device.get() +' '+selectProjectUser.get()+' '+selectDatabaseUser.get() +' '+folder.get().replace('Salvar em ','') + ''
-        result = os.system(cmd)
-        buttonConclud.set('Finalisado')
-        get_log()
+        os.system(cmd)
+        
+        cmdShared = 'comm\command.bat 8 '+device.get() +' '+selectProjectUser.get()+' '+selectSharedUser.get() +' '+folder.get().replace('Salvar em ','') + ''
+        os.system(cmdShared)
+    elif checkbuttonValue.get() == True and task.get() == 1:
+        cmd = 'comm\command.bat 5 '+device.get() +' '+folder.get().replace('Enviar ','') +''
+        os.system(cmd)
+    elif checkbuttonValue.get() == True and task.get() == 2:
+        cmd = 'comm\command.bat 9 '+device.get() +' '+folder.get().replace('Enviar ','') +' '+selectProjectUser.get() +''
+        os.system(cmd)
+    elif checkbuttonValue.get() == True and task.get() == 3:
+        cmd = 'comm\command.bat 10 '+device.get() +' '+folder.get().replace('Enviar ','') +' '+selectProjectUser.get() +''
+        os.system(cmd)
 
-    if checkbuttonValue.get() == True and task.get() == 1:
-        cmd = 'comm\command.bat 5 '+device.get() +' '+folder.get().replace('Enviar ','') + ''
-        result = os.system(cmd)
-        buttonConclud.set('Finalisado')
-        get_log()
+    buttonConclud.set('Finalisado')
+    get_log()
 
+    
 def get_log():
     with open("text/log.txt", "r") as arquivo:
-	        logs = arquivo.read()
+        logs = arquivo.read()
 
     folder.set(logs)
 
@@ -153,13 +187,18 @@ def list_database_project():
     os.system(cmd)
     build_database()
 
+def list_shared_project():
+    cmd = 'comm\command.bat 7 '+device.get() +' '+selectProjectUser.get()+''
+    os.system(cmd)
+    build_shared()
+
 def open_adb():
     clean_log()
     cmd = 'comm\command.bat 1'
     os.system(cmd)
 
     with open("text/temp.txt", "r") as arquivo:
-	    device = arquivo.read().replace('List of devices attached','').replace('unauthorized','').replace('device','')
+        device = arquivo.read().replace('List of devices attached','').replace('unauthorized','').replace('device','')
         
     for item in device.split():
         devices.append(item)
@@ -186,13 +225,22 @@ def open_project():
 def build_database():
     databaseList.clear()
     with open("text/database.txt", "r") as arquivo:
-	    database = arquivo.read()
+        database = arquivo.read()
         
     for item in database.split():
         databaseList.append(item)
     
     build_combo_database('normal')
     
+def build_shared():
+    sharedList.clear()
+    with open("text/shared.txt", "r") as arquivo:
+        shared = arquivo.read()
+        
+    for item in shared.split():
+        sharedList.append(item)
+    
+    build_combo_shared('normal')
 
 def connect_device(d):
     cmd = 'adb -s '+ d +' root'
